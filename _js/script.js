9e85b630c0d69d6ae1941e76134e2d8b071895b6
Raymon Sutedjo-The <raymon.sutedjothe@gmail.com>
2013-09-12 19:07:55 -0700

    new commit

---
+++ _js/script.js
@@ -0,0 +1,8 @@
+/*
+    Finally, let's add some interactions on the page:
+    1. When the user hovers over a navigation link, make it animate (fade out) slightly.
+    2. Make it so that the portfolio item's descriptive text is only visible after the user clicks the title.
+    3. Think about other interactions that may make sense in this context. Code them and note your reason behind those functionalities.
+*/
+
+
