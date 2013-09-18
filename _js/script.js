bdd46383051c45f4152c5dd700e00c1d7347a46b
Raymon Sutedjo-The <raymon.sutedjothe@gmail.com>
2013-09-18 00:01:05 -0700

    updated instructions

--- _js/script.js
+++ _js/script.js
@@ -1,8 +1,12 @@
 /*
     Finally, let's add some interactions on the page:
-    1. When the user hovers over a navigation link, make it animate (fade out) slightly.
+    1. When the user hovers over a menu link, lower the opacity a little.
     2. Make it so that the portfolio item's descriptive text is only visible after the user clicks the title.
     3. Think about other interactions that may make sense in this context. Code them and note your reason behind those functionalities.
+
+    ADVANCED:
+    4. Display only one portfolio item at a time, but allow the user to navigate to the next/previous item (like a slideshow). Change the html/css around if you think it'll help make the script easier to build. 
+
 */
 
 
